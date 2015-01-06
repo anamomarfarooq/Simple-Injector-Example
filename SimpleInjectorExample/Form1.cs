82e8ff4683edee7f2dab7404b77c05993ef89f60
Anam Rafique <anam.omar.farooq@gmail.com>
2015-01-06 15:44:06 +0800

    To show the understanding of Simple Injector

---
+++ SimpleInjectorExample/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SimpleInjector;
+
+
+namespace SimpleInjectorExample
+{
+    public interface IDisplay
+    {
+        void Display(string _text);
+    }
+
+    public class DisplayText : IDisplay
+    {
+        public void Display(string text)
+        {
+            MessageBox.Show("Text is: " + text);
+        }
+    }
+    public partial class Form1 : Form
+    {
+        private readonly IDisplay _display;
+
+        public Form1(IDisplay display)
+        {
+            _display = display;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _display.Display(DisplayBox.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ SimpleInjectorExample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SimpleInjector;
+
+namespace SimpleInjectorExample
+{
+    static class Program
+    {
+
+        private static Container container;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            BootStrap();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(container.GetInstance<Form1>());
+        }
+
+        private static void BootStrap()
+        { 
+            //Create a Container
+            container = new Container();
+
+            //Register Types
+            container.Register<IDisplay, DisplayText>();
+
+            //Verify container
+            container.Verify();
+
+            //Register the container class
+            Program.container = container;
+
+        }
+    }
+}
